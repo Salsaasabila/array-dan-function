158ea291a20b338fee03260690d5e1de285b039b
Salsaasabila <111962363+Salsaasabila@users.noreply.github.com>
2022-10-09 05:05:18 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int angka[]={1,3,5,7,9};
+    cout << "angka=" << angka[2]<<endl;
+    char huruf[]={'A','I','U','E','O'};
+    cout << "huruf=" << huruf[4]<<endl;
+    string nama []={"algis","osep","amel"};
+    cout << "Nama=" << nama [0]<<endl;
+    int nilai[3][4]={
+    {80,85}, 
+    {90,92,94,96}, 
+    {75,78,79}
+    };
+    cout << "nilai=" <<nilai[1][0] <<endl;
+}
---
+++ function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void say_hello (string nama){
+     cout << "Hello, " << nama << endl;
+}
+
+void tambah (int angka1, int angka2){
+     printf ("Perjumlahan %d + %d = %d",
+     angka1, angka2, angka1 + angka2);
+}
+
+int main(){
+  
+    say_hello ("Salsa");
+    say_hello ("Arezz");
+    tambah (10, 5);
+    tambah (20, 10);
+    tambah (25, 25);
+}
+   
